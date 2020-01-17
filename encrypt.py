a3591bd831dcb80cb32752d00790537fa181a23a
Ananye Agarwal <ananayagarwal@gmail.com>
2020-01-17 13:24:26 +0530

    Add the encrypt script

--- encrypt.py
+++ encrypt.py
@@ -1,6 +1,72 @@
-from utils import read_matrix
+from utils import *
 
 key_filename = "key.in"
+plaintext_filename = "plaintext.in"
+ciphertext_filename = "ciphertext.out"
 
+# Read the key as a matrix
 key = read_matrix("key.in")
-print(key)
+key_length = len(key)
+
+# Create file handles for plaintext and ciphertext
+plaintext_fh = open(plaintext_filename, 'r')
+ciphertext_fh = open(ciphertext_filename, 'w')
+
+# Read the plaintext
+plaintext = plaintext_fh.read()
+plaintext_fh.close()
+
+# Create an empty ciphertext character array
+ciphertext = []
+
+# Iterate over the plaintext
+curr_idx = 0
+while curr_idx < len(plaintext):
+    # Create the string to be processed with x's
+    curr_text = [24 for i in range(key_length)]
+    # Keep track of the positions at which character should be inserted into the ciphertext
+    insert_positions = []
+    # Count the number of characters read from the plaintext
+    read_ctr = 0
+    # Build up curr_text
+    while(read_ctr < key_length and curr_idx < len(plaintext)):
+        if (plaintext[curr_idx].isalpha()):
+            # This is a character
+            curr_text[read_ctr] = char2num(plaintext[curr_idx])
+            # Put a dummy character into the ciphertext
+            ciphertext.append('x')
+            insert_positions.append(curr_idx)
+            # Increment both counters
+            read_ctr += 1
+            curr_idx += 1
+        else:
+            # Copy the plaintext character into the ciphertext
+            ciphertext.append(plaintext[curr_idx])
+            curr_idx += 1
+
+    # Do matrix multiplication mod 26 and get encrypted text
+    encrypted_text = matrix_mult(key, curr_text, 26)
+
+    # Place the text into the ciphertext
+    for i in range(read_ctr):
+        ciphertext[insert_positions[i]] = num2char(encrypted_text[i])
+    
+    # Append the residual ciphertext
+    for i in range(read_ctr, key_length):
+        ciphertext.append(num2char(encrypted_text[i]))
+
+# Convert the ciphertext to a string
+ciphertext = ''.join(ciphertext)
+
+# Write the ciphertext to file
+ciphertext_fh.write(ciphertext)
+ciphertext_fh.close()
+
+    
+
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -18,3 +18,25 @@ def read_matrix(filename):
             ctr+=1
         s.append(tmp)
     return s
+
+# Convert a character to a number mod 26
+def char2num(c):
+    return ord(c.lower()) - ord('a')
+
+# Convert a number mod 26 to a character 
+def num2char(n):
+    return chr(n + ord('a'))
+
+def matrix_mult(mat, vec, m):
+    n = len(mat)
+    assert(len(vec) == n)
+    # Create the result vector
+    res = [0 for i in range(n)]
+    for i in range(n):
+        # Create a row sum variable
+        row_sum = 0 
+        for j in range(n):
+            row_sum += mat[i][j] * vec[j]
+            row_sum %= m
+        res[i] = row_sum
+    return res
