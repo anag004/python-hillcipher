32b247b338105aec1954b2ce29b547e5140984aa
Ananye Agarwal <ananayagarwal@gmail.com>
2020-01-25 13:42:19 +0530

    The plaintext and ciphertext are now inputted through stdin

--- decrypt.py
+++ decrypt.py
@@ -1,8 +1,6 @@
 from utils import *
 
 key_filename = "key.in"
-plaintext_filename = "plaintext.out"
-ciphertext_filename = "ciphertext.in"
 padding_letter = 'z'
 
 # Read the key as a matrix
@@ -12,19 +10,12 @@ key_length = len(key)
 # Get the inverse of the key
 key_inverse = matrix_inverse(key, 26)
 
-# Create file handles for plaintext and ciphertext
-plaintext_fh = open(plaintext_filename, 'w')
-ciphertext_fh = open(ciphertext_filename, 'r')
-
 # Read the ciphertext
-ciphertext = ciphertext_fh.read()
-ciphertext_fh.close()
+ciphertext = raw_input()
 
 # Create an empty plaintext character array
 plaintext = []
 
 plaintext = apply(key_inverse, ciphertext, padding_letter)
 
-# Write the ciphertext to file
-plaintext_fh.write(plaintext)
-plaintext_fh.close()
+print(plaintext)
--- encrypt.py
+++ encrypt.py
@@ -1,28 +1,16 @@
 from utils import *
 
 key_filename = "key.in"
-plaintext_filename = "plaintext.in"
-ciphertext_filename = "ciphertext.out"
 padding_letter = 'z'
 
 # Read the key as a matrix
 key = read_matrix("key.in")
 key_length = len(key)
 
-# Create file handles for plaintext and ciphertext
-plaintext_fh = open(plaintext_filename, 'r')
-ciphertext_fh = open(ciphertext_filename, 'w')
-
-# Read the plaintext
-plaintext = plaintext_fh.read()
-plaintext_fh.close()
+plaintext = raw_input()
 
 ciphertext = apply(key, plaintext, padding_letter)
 print(ciphertext)
-
-# Write the ciphertext to file
-ciphertext_fh.write(ciphertext)
-ciphertext_fh.close()
 
     
 
